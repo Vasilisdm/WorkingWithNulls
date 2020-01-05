1d427dca29fbdc2d6fe6226d2c619f0a65fc1d2f
vasilisdmr <vasilisdmr@gmail.com>
2020-01-05 14:34:11 +0200

    Added the class wich is going to display player's properties to console, also checked if the player has been logged in before, and if he had setted the birthdate.

---
+++ GameConsole/PlayerDisplayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace GameConsole
+{
+    public class PlayerDisplayer
+    {
+        public static void Write(PlayerCharacter player)
+        {
+            Console.WriteLine($"Player Name: {player.Name}");
+
+            if (player.DaysSinceLastLogin == -1)
+            {
+                Console.WriteLine("No value for DaysSinceLastLogin");
+            }
+            else
+            {
+                Console.WriteLine($"Days since last login: {player.DaysSinceLastLogin}");
+            }
+
+            if (player.DateOfBirth == DateTime.MinValue)
+            {
+                Console.WriteLine("No date specified for birthdate");
+            }
+            else
+            {
+                Console.WriteLine($"Player's birthdate: {player.DateOfBirth}");
+            }
+        }
+    }
+}
