6828a89b4b424ae472afc113b040107f274c8794
vasilisdmr <vasilisdmr@gmail.com>
2020-01-06 18:36:27 +0200

    Added DiamondSkinDefense wich also implements ISpecialDefense.

---
+++ GameConsole/DiamondSkinDefense.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GameConsole
+{
+    public class DiamondSkinDefense : ISpecialDefense
+    {
+        public int CalculateDamageReduction(int totalDamage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
