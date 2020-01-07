8ebe1971f187d982e01781433378c873039b4056
vasilisdmr <vasilisdmr@gmail.com>
2020-01-07 21:59:57 +0200

    Enabled nullable features to show the appearance of an error if the a null reference type is unitiliazed.

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -9,10 +9,13 @@ namespace GameConsole
             PlayerCharacter orc = new PlayerCharacter(new IronBonesDefence()) { Name = "Undala" };
             PlayerCharacter mage = new PlayerCharacter(new DiamondSkinDefence()) { Name = "Nuromancer" };
             PlayerCharacter dwarf = new PlayerCharacter(SpecialDefence.Null) { Name = "Balin" };
+            PlayerCharacter empty = new PlayerCharacter(SpecialDefence.Null) { };
 
             orc.Hit(10);
             mage.Hit(10);
             dwarf.Hit(10);
+
+            Console.WriteLine(empty);
 
             Console.ReadLine();
         }
